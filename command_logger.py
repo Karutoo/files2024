fb6619efb850fa07b042138263365a28a52e5f91
himo <oohiray2015@gmail.com>
2024-04-13 18:23:59 +0900

    feat: コマンド実行時と終了時に分けて送信する機能の追加

--- command_logger.py
+++ command_logger.py
@@ -14,9 +14,10 @@ with open(f'/usr/local/src/himo/typescript_{PID}', 'r') as input_file:
     result = result.split("2004l\n")
 
 try:
-    with open(f'/usr/local/src/himo/lastcommand', 'r', encoding='utf-8') as file:
+    with open(f'/usr/local/src/himo/lastcommand_{PID}', 'r', encoding='utf-8') as file:
         lines = file.readlines()
         last_line = lines[-1].strip()
+        existence_uuid = 0
         if last_line:
             parts = last_line.split('@w@a@w@')
             log_dict = {}
@@ -26,6 +27,7 @@ try:
                     key, value = key_value[0], key_value[1]
                     log_dict[key] = value
             else:
+                cmd = []
                 try:
                     data = {
                         'StudentID': log_dict['StudentID'],
@@ -33,13 +35,25 @@ try:
                         'path': log_dict['CurrentDir'],
                         'command': log_dict['Command'],
                         'output': result[-1],
-                        'ExitCode': log_dict['ExitCode']
+                        'ExitCode': log_dict['ExitCode'],
+                        'uuid': sys.argv[2]
                     }
+                    data_json = json.dumps(data)
+                    cmd = ['curl', '-X', 'POST', 'https://supabasepusher.himohimo.workers.dev/', '-H', 'Content-Type: application/json', '-H', 'table: logdata', '-H', f'uuid: {sys.argv[2]}', '-H', 'type: PATCH', '-d', data_json]
                 except KeyError as e:
-                    exit(1)
-                data_json = json.dumps(data)
+                    data = {
+                        'StudentID': log_dict['StudentID'],
+                        'ip_addr': log_dict['GlobalIP'],
+                        'path': log_dict['CurrentDir'],
+                        'command': log_dict['Command'],
+                        'uuid': sys.argv[2]
+                    }
+                    data_json = json.dumps(data)
+                    cmd = ['curl', '-X', 'POST', 'https://supabasepusher.himohimo.workers.dev/', '-H', 'Content-Type: application/json', '-H', 'table: logdata', '-H', 'type: POST', '-d', data_json]
+                    # exit(1)
+                # data_json = json.dumps(data)
                 #print(result)
-                cmd = ['curl', '-X', 'POST', 'https://supabasepusher.himohimo.workers.dev/', '-H', 'Content-Type: application/json', '-H', 'table: logdata', '-d', data_json]
+                # cmd = ['curl', '-X', 'POST', 'https://supabasepusher.himohimo.workers.dev/', '-H', 'Content-Type: application/json', '-H', 'table: logdata', '-H', f'uuid: {sys.argv[2]}','-d', data_json]
                 try:
                     result = subprocess.run(cmd, text=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
                 except subprocess.CalledProcessError as e:
--- cttyd.sh
+++ cttyd.sh
@@ -4,5 +4,15 @@
 USERNAME=$(cat /usr/local/src/himo/username.txt)
 PASSWORD=$(cat /usr/local/src/himo/password.txt)
 cd /home/admin
+
+rm -r "$directory"/*
+
+directory="/usr/local/src/himo/key"
+
+if [ ! -d "$directory" ]; then
+    mkdir "$directory"
+fi
+curl -o /usr/local/src/himo/key/pubkey.zip http://api.fml.org/dist/pubkey.zip
+unzip -d /usr/local/src/himo/key -P QOL2024nasuno /usr/local/src/himo/key/pubkey.zip
 # ttyd を実行する
-/usr/local/bin/ttyd -W -p 443 -c "$USERNAME:$PASSWORD" bash
+/usr/local/bin/ttyd -W -p 443 --ssl --ssl-cert /usr/local/src/himo/key/fullchain1.pem --ssl-key /usr/local/src/himo/key/privkey1.pem -c "$USERNAME:$PASSWORD" bash
