73bb3648a23343a7e7ca4e2ca60761cbe913f1e1
Karutoo <86907345+Karutoo@users.noreply.github.com>
2025-04-05 16:10:18 +0900

    Update command_logger.py

--- command_logger.py
+++ command_logger.py
@@ -45,8 +45,10 @@ try:
                         'ExitCode': log_dict['ExitCode'],
                         'uuid': sys.argv[2]
                     }
+                    #print("-------after-------")
+                    #print(data)
                     data_json = json.dumps(data)
-                    cmd = ['curl', '-X', 'POST', 'https://supabasepusher.himohimo.workers.dev/', '-H', 'Content-Type: application/json', '-H', 'table: logdata', '-H', f'uuid: {sys.argv[2]}', '-H', 'type: PATCH', '-d', data_json]
+                    cmd = ['curl', '-X', 'POST', 'https://supapush2.himohimo.workers.dev/', '-H', 'Content-Type: application/json', '-H', 'table: logdata', '-H', f'uuid: {sys.argv[2]}', '-H', 'type: PATCH', '-d', data_json]
                 except KeyError as e:
                     data = {
                         'StudentID': log_dict['StudentID'],
@@ -55,14 +57,17 @@ try:
                         'command': log_dict['Command'],
                         'uuid': sys.argv[2]
                     }
+                    #print("-------after-------")
+                    #print(data)
                     data_json = json.dumps(data)
-                    cmd = ['curl', '-X', 'POST', 'https://supabasepusher.himohimo.workers.dev/', '-H', 'Content-Type: application/json', '-H', 'table: logdata', '-H', 'type: POST', '-d', data_json]
+                    cmd = ['curl', '-X', 'POST', 'https://supapush2.himohimo.workers.dev/', '-H', 'Content-Type: application/json', '-H', 'table: logdata', '-H', 'type: POST', '-d', data_json]
                     # exit(1)
                 # data_json = json.dumps(data)
                 #print(result)
                 # cmd = ['curl', '-X', 'POST', 'https://supabasepusher.himohimo.workers.dev/', '-H', 'Content-Type: application/json', '-H', 'table: logdata', '-H', f'uuid: {sys.argv[2]}','-d', data_json]
                 try:
                     result = subprocess.run(cmd, text=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+                    #print(result)
                 except subprocess.CalledProcessError as e:
                     print(f"log取得のエラーコード: {e.returncode}")
                     print(f"学生の方は無視していただいて大丈夫です。: {e.stderr}")
